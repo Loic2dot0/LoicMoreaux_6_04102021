708948cf7230f77fab22eacc98824bf35ec5565a
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-04 18:10:22 +0200

    app.js: ajout de la connexion MongDB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Loic2dot0:n4T3PV6Rn7x7kpE@cluster0.vjlu7.mongodb.net/piiquanteDB?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Réussite de la connexion MongoDB'))
+    .catch(() => console.log('Echec de la connexion MongoDB'));
 
 const app = express();
 
