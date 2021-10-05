ff577f4e2315ec131d5667c55948cad53bad75f0
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-05 17:29:46 +0200

    app.js: ajout du middleware express.json(). controllers/user.js:  création de la fonction signup, import de bcrypt et jsonwebtoken

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json());
+
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,22 @@
-const User = require('../models/User');
+const bcrypt = require('bcrypt'); // Ajout de bcrypt pour le chriffrage du mot de passe
+const salt = 10; //Nombre de tour pour le salage du mot du hash
+const jwt = require('jsonwebtoken'); // Ajout de jsonwebtoken pour la gestion des token
+
+const User = require('../models/User'); //import du modèle user
+
 
 exports.signup = (req, res, next) => {
-    res.json({ message: 'Create User'});
+    bcrypt.hash(req.body.password, salt) //méthode asynchrone pour le hash du password
+        .then(hash =>{
+            const user = new User({ // création du nouvel utilisateur
+                email: req.body.email,
+                password: hash
+            });
+            user.save() // sauvegarde dans la DB du nouvel utilisateur
+                .then(() => res.status(201).json({message: "User created"}))
+                .catch(error => res.status(400).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
 };
 
 exports.login = (req, res, next) => {
