fb22a015c3699f275144de8e841c44748038c4f4
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-07 16:22:11 +0200

    controller/sauce.js: ajout du contenu de la fonction getOneSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,12 +2,15 @@ const Sauce = require('../models/Sauce'); //import du modèle sauce
 
 
 exports.getAllSauce = (req, res, next) => {
-    Sauce.find() //on cherche tous les modèles Sauce dans la DB
+    Sauce.find() //on recherche tous les modèles Sauce dans la DB
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({error}));
 };
 
 exports.getOneSauce = (req, res, next) => {
+    Sauce.findById(req.params.id) //on recherche une sauce par son Id
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(400).json({error}));
 };
 
 exports.createSauce = (req, res, next) => {
