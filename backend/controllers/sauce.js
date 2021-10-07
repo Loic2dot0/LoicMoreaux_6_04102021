0fe77fdc687a78c15317b07d0331d66dfd798444
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-07 12:35:41 +0200

    controllers/sauce.js : Création du fichier, déclaration des fonctions

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,20 @@
+const Sauce = require('../models/Sauce'); //import du modèle sauce
+
+
+exports.getAllSauce = (req, res, next) => {
+};
+
+exports.getOneSauce = (req, res, next) => {
+};
+
+exports.createSauce = (req, res, next) => {
+};
+
+exports.modifySauce = (req, res, next) => {
+};
+
+exports.deleteSauce = (req, res, next) => {
+};
+
+exports.rateSauce = (req, res, next) => {
+};
