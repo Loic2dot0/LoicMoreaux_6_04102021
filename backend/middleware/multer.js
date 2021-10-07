f29944654134604faabde9c8650a0afe737675ab
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-07 18:41:00 +0200

    Middleware/multer.js :  création du fichier pour la gestion de la prise ne charge de l'envoie de fichier. routes/sauces.js : ajout de multer dans les routes post et put

---
+++ backend/middleware/multer.js
@@ -0,0 +1,20 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg' : 'jpg',
+    'image/jpeg' : 'jpg',
+    'image/png' : 'png'
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) =>{
+        callback(null, 'images') //indique le dossier ou enregistrer les fichiers
+    },
+    filename: (req, file, callback) => {
+        const name = Date.now() + '_' + Math.floor(Math.random()*10000); //définition du nom de fichier unique avec un timestamp + un nbre aléatoire
+        const extension = MIME_TYPES[file.mimetype]; // création de l'extension a partir du type MIME du fichier
+        callback(null, name + '.' + extension);
+    }
+})
+
+module.exports = multer({storage}).single('image');
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce'); //import du fichier controller
+const multer = require('../middleware/multer'); //import du fichier multer
 
 router.get('/', sauceCtrl.getAllSauce);
 router.get('/:id', sauceCtrl.getOneSauce);
-router.post('/', sauceCtrl.createSauce);
-router.put('/:id', sauceCtrl.modifySauce);
+router.post('/', multer, sauceCtrl.createSauce);
+router.put('/:id', multer, sauceCtrl.modifySauce);
 router.delete('/:id', sauceCtrl.deleteSauce);
 router.post('/:id/like', sauceCtrl.rateSauce);
 
