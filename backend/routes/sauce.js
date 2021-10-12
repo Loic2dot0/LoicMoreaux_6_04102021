e7ba782bf1c2cb3cc7c2f80161c742dc1969b8ea
Loic2dot0 <moreaux.loic@gmail.com>
2021-10-12 15:52:55 +0200

    routes/sauce.js : ajout de l'authemtification dans les routes. .gitgnore : ajouter du dossier images

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce'); //import du fichier controller
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer'); //import du fichier multer
 
-router.get('/', sauceCtrl.getAllSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.post('/', multer, sauceCtrl.createSauce);
-router.put('/:id', multer, sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
-router.post('/:id/like', sauceCtrl.rateSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.rateSauce);
 
 module.exports = router;
